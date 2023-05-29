6ca090d6eb5373010d73ea183aefe1ed7d65a923
Mykola Vorotynskyy <vorotynskyy@gmail.com>
2023-05-29 14:29:27 +0200

    commit

---
+++ tic_tac_toe/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Game from './component/Game';
+import Board from './component/Board';
+import './App.css';
+const App = () => <Board />
+
+export default App;
---
+++ tic_tac_toe/src/component/Game.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Board from "./Board";
+
+const Game = () => { 
+    <Board />
+    const handleClick = () => {
+
+    }
+
+    const jumpTo = () => {
+
+    }
+
+    const renderMoves = () => {
+
+    }
+}
+
+export default Game;
